71b6c7de38459cd0964bf878da63f76e9aeaebf5
Jens Henske <67688157+JtheK1984@users.noreply.github.com>
2025-02-05 15:58:59 +0100

    Version 1.6

--- PrepareCopy.cmd
+++ PrepareCopy.cmd
@@ -1,10 +1,5 @@
 echo "Kopiere Datei ins Setupverzeichnis"
-copy /y /v Win32\Release\PCMBenutzerverwaltung.exe "e:\Inno\Setupfiles\Programme\PCMBenutzerverwaltung"
-copy /y /v Win64\Release\PCMBenutzerverwaltung.exe "e:\Inno\Setupfiles\Programme\PCMBenutzerverwaltung_x64"
+copy /y /v Win32\Release\PCMDevManager.exe "e:\Inno\Setupfiles\Programme\PCMDevManager"
+copy /y /v Win64\Release\PCMDevManager.exe "e:\Inno\Setupfiles\Programme\PCMDevManager_x64"
 
-copy /y /v Win64\Release\PCMBenutzerverwaltung.DE "e:\Inno\Setupfiles\Programme\PCMBenutzerverwaltung_x64"
-copy /y /v Win64\Release\PCMBenutzerverwaltung.EN "e:\Inno\Setupfiles\Programme\PCMBenutzerverwaltung_x64"
 
-copy /y /v PCMBenutzerverwaltung.pdf "e:\Inno\Setupfiles\Programme\PCMBenutzerverwaltung"
-copy /y /v PCMBenutzerverwaltung.docx "e:\Inno\Setupfiles\Programme\PCMBenutzerverwaltung"
-copy /y /v PCMBenutzerverwaltung.htm "e:\Inno\Setupfiles\Programme\PCMBenutzerverwaltung"
